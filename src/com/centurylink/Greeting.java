8f69c1f74e4aeaa21c549beb4ca0f42bbfb5c439
visheshmohan <63037782+visheshmohan@users.noreply.github.com>
2020-05-29 13:20:01 +0530

    Greeting.java

---
+++ src/com/centurylink/Greeting.java
@@ -0,0 +1,11 @@
+package com.centurylink;
+
+public class Greeting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("C'est Bonjour");
+	}
+
+}
